78001743d0c19eeca577a5f3b2e06f2a8a313d57
Francois Gerthoffert <f.gerthoffert@gmail.com>
2020-07-16 21:46:17 -0400

    Updated dependencies to zindexer components

--- src/config.module.ts
+++ src/config.module.ts
@@ -1,12 +1,6 @@
 import { Module, DynamicModule } from '@nestjs/common';
 import { ConfigService } from './config.service';
 
-// @Module({
-//   providers: [ConfigService],
-//   exports: [ConfigService],
-// })
-// export class ConfigModule {}
-
 @Module({})
 export class ConfigModule {
   static register(): DynamicModule {
